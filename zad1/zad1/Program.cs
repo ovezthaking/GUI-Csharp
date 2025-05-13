1e8d232674f39832f9c7c13a62e551594067563f
ovezthaking <oliwerx12@gmail.com>
2025-05-13 03:04:55 +0200

    zad1

---
+++ zad1/zad1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj liczbę naturalną: ");
+        string input = Console.ReadLine();
+        int N = int.Parse(input);
+        int maxProduct = N * N;
+        int maxDigits = maxProduct.ToString().Length;
+        int fieldWidth = maxDigits + 1;
+        
+        for (int i = 1; i <= N; i++)
+        {
+            for (int j = 1; j <= N; j++)
+            {
+                int product = i * j;
+                Console.Write("{0," + fieldWidth + "}", product);
+            }
+            Console.WriteLine();
+        }
+    }
+}
