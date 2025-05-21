22a061e274d36e29e5f085a22d06f5872ff407ec
Oliwer Urbaniak <341479@uwr.edu.pl>
2025-05-21 11:32:34 +0200

    L3 in progress

--- Lista 3/Program.cs
+++ Lista 3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿public class Obserwator
+{
+
+}
+
+public class Tworca {
+    
+    
+}
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
