e8637b34c76c06c0d5f2da9c0b52cc46b22fa3f4
ovezthaking <oliwerx12@gmail.com>
2025-05-21 20:27:33 +0200

    in progressss

--- Lista 3/Program.cs
+++ Lista 3/Program.cs
@@ -1,6 +1,24 @@
-﻿public class Obserwator
-{
+﻿
+public delegate void NowyObserwatorDelegate(string nazwa, double x, double y);
 
+public delegate WypiszDelegate();
+
+public class Obserwator
+{
+    private string nazwa;
+    private double x;
+    private double y;
+
+    private List<(string Nazwa, double X, double Y, double Odleglosc)> sasiedzi;
+    public Obserwator(string nazwa, double x, double y)
+    {
+        this.nazwa = nazwa;
+        this.x = x;
+        this.y = y;
+        this.sasiedzi = new List<(string, double, double, double)>();
+    }
+
+    
 }
 
 public class Tworca {
@@ -8,5 +26,19 @@ public class Tworca {
     
 }
 
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Tworzenie instancji klasy Tworca
+        Tworca tworca = new Tworca();
+
+        // Tworzenie instancji klasy Obserwator
+        Obserwator obserwator = new Obserwator("minecraft", 0.5, 0.5);
+
+        // Możesz dodać logikę do interakcji między twórcą a obserwatorem tutaj
+        Console.WriteLine("Hello, World!");
+    }
+}
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
